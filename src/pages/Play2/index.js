60898330e4d5cd90905f5c9357dd19f8604c6eb0
Gthalles <garbelottithalles@gmail.com>
2022-05-02 23:10:29 -0300

    Implements play screens.

---
+++ src/pages/Play2/index.js
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { Button } from "../../components/Button";
+import { Background } from "../../components/Background";
+import { Header } from "../../components/Header";
+import { Flashcard } from "../../components/Flashcard";
+
+// eslint-disable-next-line react/prop-types
+export function Play2 ({ navigation }) {
+	const styles = StyleSheet.create({
+		topContainer: {
+			marginTop: 60,
+		},
+		instructionText: {
+			color: "#FFF",
+			fontSize: 18,
+			marginBottom: 30,
+			textAlign: "center"
+		},
+		buttonContainer: {
+			width: "80%",
+			height: 155,
+			marginTop: 50,
+			marginBottom: 50,
+		},
+	});
+
+	return (
+		<View>
+			<Header title="Coleção - Objetos" navigation={ navigation }/>
+			<Background>
+					<View style={ styles.topContainer }>
+						<Text style={ styles.instructionText }>Cartão 8/8</Text>
+					</View>
+					<Flashcard text="Janela" />
+					<View style={ styles.buttonContainer }>
+						<Button text="VIRAR" color="#6A61A1" />
+					</View>
+			</Background>
+		</View>
+	);
+}
---
+++ src/pages/Play3/index.js
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { Button } from "../../components/Button";
+import { Background } from "../../components/Background";
+import { Header } from "../../components/Header";
+import { LargeFlashcard } from "../../components/LargeFlashcard";
+
+// eslint-disable-next-line react/prop-types
+export function Play3 ({ navigation }) {
+	const styles = StyleSheet.create({
+		topContainer: {
+			marginTop: 60,
+		},
+		instructionText: {
+			color: "#FFF",
+			fontSize: 18,
+			marginBottom: 30,
+			textAlign: "center"
+		},
+		buttonContainer: {
+			width: "80%",
+			height: 175,
+			marginTop: 50,
+			marginBottom: 50,
+		},
+	});
+
+	return (
+		<View>
+			<Header title="Coleção - Objetos" navigation={ navigation }/>
+			<Background>
+					<View style={ styles.topContainer }>
+						<Text style={ styles.instructionText }>Cartão 8/8</Text>
+					</View>
+					<LargeFlashcard frontText="Janela" backText="Window" />
+					<View style={ styles.buttonContainer }>
+						<Button text="FINALIZAR" color="#61A170" />
+					</View>
+			</Background>
+		</View>
+	);
+}
